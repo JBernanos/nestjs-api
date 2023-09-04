7808c5f1e1dedf4bb9119c5116479ece7fe1bc7c
JBernanos <jp.bf@icloud.com>
2023-09-04 18:10:39 -0300

    build: mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
